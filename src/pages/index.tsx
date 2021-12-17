9e02878f5d967a1e1ae8b969be51e252f5d85714
Cesar Mazzaro <cesar_mazzaro1@hotmail.com>
2021-12-17 16:49:13 -0300

    SubscribeButton created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from './home.module.scss'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 monthly</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
